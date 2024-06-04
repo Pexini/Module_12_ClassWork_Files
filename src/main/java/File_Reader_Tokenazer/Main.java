9bfe6da919fd8f611d15262e4098f05bd1aac39c
Pavel Davydov <Davydov.works@gmail.com>
2024-06-04 02:23:56 -0400

    tokenaizer

--- src/main/java/File_Reader_Tokenazer/Main.java
+++ src/main/java/File_Reader_Tokenazer/Main.java
@@ -1,10 +1,33 @@
 package File_Reader_Tokenazer;
 
-import java.io.File;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        File in = new File("Text.txt");
-        File out = new
+        File in = new File("resources/In.txt");
+        File out = new File("resources/out.txt");
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(in)));
+                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(out)));) {
+
+            bufferedReader.lines().forEach(x -> {
+                StringTokenizer stringTokenizer = new StringTokenizer(x, "Java", true);
+                while (stringTokenizer.hasMoreTokens()) {
+                    printWriter.println("Token: " + stringTokenizer.nextToken());
+                }
+
+
+//            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+//            while (stringTokenizer.hasMoreTokens()){
+//                System.out.println(stringTokenizer.nextToken());
+            });
+        } catch (FileNotFoundException e) {
+            System.out.print("File not found");
+        } catch (IOException e) {
+            System.out.print("Wrong File");
+        }
     }
+
 }
+
