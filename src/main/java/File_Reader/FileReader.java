e9a0e2f75eb6970f0011e040f807d7f84554bbb3
Pavel Davydov <Davydov.works@gmail.com>
2024-06-04 01:56:28 -0400

    tokenaizer

--- src/main/java/File_Reader/FileReader.java
+++ src/main/java/File_Reader/FileReader.java
@@ -5,7 +5,8 @@ import java.io.*;
 public class FileReader {
     public static void main(String[] args) {
         File file = new File("Set_of_Numbers.txt");
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));) {
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));) {
             StringBuilder stringBuilder = new StringBuilder();
             bufferedReader.lines().forEach(x -> stringBuilder.append(x + "\n"));
             System.out.print(stringBuilder.toString());
---
+++ src/main/java/File_Reader_Tokenazer/Main.java
@@ -0,0 +1,10 @@
+package File_Reader_Tokenazer;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File in = new File("Text.txt");
+        File out = new
+    }
+}
